728941312d651cfecd693d9cd4d5cde501edd15b
DESKTOP-SON36M3\01 <wjsdbqja156@gmail.com>
2024-12-27 15:40:01 +0900

    진짜 최종

--- main/java/Link/Link.java
+++ main/java/Link/Link.java
@@ -6,51 +6,47 @@ public class Link {
 		String Link = "";
 			switch(str) {
 				case "교육기획": {
-					Link = "./Education.jsp";
+					Link = "./Bannerpage.jsp";
 					break;
 				}
-				case "조직관리": {
+				case "인사관리": {
 					Link = "./Human_Resource.jsp";
 					break;
 				}
 				case "회계": {
-					Link = "./Main.jsp";
+					Link = "./Accounting.jsp";
 					break;
 				}
 				case "자재현황분석": {
-					Link = "./Main.jsp";
+					Link = "./Material_Status_Analyze.jsp";
 					break;
 				}
 				case "생산현황분석": {
-					Link = "./Main.jsp";
+					Link = "./Production_Profile.jsp";
 					break;
 				}
 				case "재고관리": {
-					Link = "./Main.jsp";
+					Link = "./Inventory.jsp";
 					break;
 				}
 				case "CEO": {
 					Link = "./Main.jsp";
 					break;
 				}
-				case "불량품관리": {
-					Link = "./Main.jsp";
-					break;
-				}
 				case "생산기획": {
-					Link = "./Main.jsp";
+					Link = "./Production_Status.jsp";
 					break;
 				}
 				case "품질관리": {
-					Link = "./Main.jsp";
+					Link = "./Quality_Control.jsp";
 					break;
 				}
 				case "매장관리": {
-					Link = "./Main.jsp";
+					Link = "./Correspondent_Product.jsp";
 					break;
 				}
-				case "CS": {
-					Link = "./Main.jsp";
+				case "자재 거래처 관리": {
+					Link = "./Correspondent_Material.jsp";
 					break;
 				}
 				
